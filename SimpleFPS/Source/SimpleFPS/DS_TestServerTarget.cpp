50e8afec4e1226442bec763409462da6ba5387ea
sn.Lee <ssung1301@naver.com>
2025-03-05 17:45:51 +0900

    wip

---
+++ SimpleFPS/Source/SimpleFPS/DS_TestServerTarget.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DS_TestServerTarget.h"
+
+DS_TestServerTarget::DS_TestServerTarget()
+{
+}
+
+DS_TestServerTarget::~DS_TestServerTarget()
+{
+}
---
+++ SimpleFPS/Source/SimpleFPS/DS_TestServerTarget.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SIMPLEFPS_API DS_TestServerTarget
+{
+public:
+	DS_TestServerTarget();
+	~DS_TestServerTarget();
+};
